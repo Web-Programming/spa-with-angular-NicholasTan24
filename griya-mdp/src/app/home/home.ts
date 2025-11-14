ab4f1283dcd69ec2aa805937efebba4b00fb0b0e
NicholasTan24 <nicholastan_2327240005@mhs.mdp.ac.id>
2025-11-14 17:00:20 +0700

    error
    implementasi lanjutan

--- griya-mdp/src/app/home/home.ts
+++ griya-mdp/src/app/home/home.ts
@@ -1,6 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
-import { LokasiPerumahan } from '../lokasi-perumahan/lokasi-perumahan';
+import { Housing, LokasiPerumahan } from '../lokasi-perumahan/lokasi-perumahan';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -9,21 +9,25 @@ import { CommonModule } from '@angular/common';
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
-export class Home {
-  // Array untuk data perumahan (bisa diisi dari backend nanti)
-  housingList = [
-    {
-      id: 1,
-      name: 'Griya Asri Residence',
-      location: 'Jakarta Selatan',
-      price: 850000000,
-      bedrooms: 3,
-      bathrooms: 2,
-      area: 120,
-      image: 'https://images.unsplash.com/photo-1568605114967-8130f3a36994',
-      rating: 4.5,
-      status: 'Available'
-    },
-    // ... data lainnya
-  ];
+export class Home implements OnInit {
+  housingList: Housing[] = [...];
+  filteredList: Housing[] = [];
+  selectedFilter: string = 'all';
+  
+  ngOnInit() {
+    this.filteredList = [...this.housingList];
+  }
+  
+  filterByType(type: string) {
+    this.selectedFilter = type;
+    if (type === 'all') {
+      this.filteredList = [...this.housingList];
+    } else {
+      this.filteredList = this.housingList.filter(h => h.type === type);
+    }
+  }
+  
+  isFilterActive(type: string): boolean {
+    return this.selectedFilter === type;
+  }
 }
--- griya-mdp/src/app/lokasi-perumahan/lokasi-perumahan.ts
+++ griya-mdp/src/app/lokasi-perumahan/lokasi-perumahan.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -8,6 +8,46 @@ import { CommonModule } from '@angular/common';
   templateUrl: './lokasi-perumahan.html',
   styleUrl: './lokasi-perumahan.css'
 })
+export interface Housing {
+  id: number;
+  name: string;
+  location: string;
+  price: number;
+  bedrooms: number;
+  bathrooms: number;
+  area: number;
+  image: string;
+  rating: number;
+  status: string;
+  type?: string;
+  description?: string;
+  postedDays?: number;
+}
+
 export class LokasiPerumahan {
-  // Nanti bisa ditambahkan @Input() untuk menerima data dari parent
+  @Input() housing: Housing = { /* default values */ };
+  
+  getStars(): number[] {
+    return Array(Math.floor(this.housing.rating)).fill(0);
+  }
+  
+  hasHalfStar(): boolean {
+    return this.housing.rating % 1 >= 0.5;
+  }
+
+   getEmptyStars(): number[] {
+    const fullStars = Math.floor(this.housing.rating);
+    const hasHalf = this.hasHalfStar() ? 1 : 0;
+    const emptyStars = 5 - fullStars - hasHalf;
+    return Array(emptyStars).fill(0);
+  }
+
+   // Format harga ke Rupiah
+  formatPrice(price: number): string {
+    return new Intl.NumberFormat('id-ID', {
+      style: 'currency',
+      currency: 'IDR',
+      minimumFractionDigits: 0
+    }).format(price);
+  }
 }
